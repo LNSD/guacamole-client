d8bbbe898c393b920350b600258244889d98b49c
Lorenzo Delgado <ldelgado@zscaler.com>
2021-12-27 03:23:40 +0100

    protocol: added instruction constructors

---
+++ libs/protocol/src/lib/instructions/server-handshake.ts
@@ -0,0 +1,20 @@
+import { Instruction } from "./instruction";
+
+/**
+ * Reports the expected format of the argument list for the protocol requested by the client. This
+ * message can be sent by the server during the handshake phase only.
+ *
+ * The first parameter of this instruction will be the protocol version supported by the server.
+ * This is used to negotiate protocol compatibility between the client and the server, with the
+ * highest supported protocol by both sides being chosen. Versions of Guacamole prior to 1.1.0 do
+ * not support protocol version negotiation, and will silently ignore this instruction.
+ *
+ * The remaining parameters of the args instruction are the names of all connection parameters
+ * accepted by the server for the protocol selected by the client, in order. The client's responding
+ * connect instruction must contain the values of each of these parameters in the same order.
+ *
+ * @param version - The protocol version supported by the server.
+ * @param params - The names of all connection parameters accepted by the server for the protocol
+ *                 selected by the client.
+ */
+export const args = (version: string, ...params: string[]): Instruction => ['args', version, ...params];
