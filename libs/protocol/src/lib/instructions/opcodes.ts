de8636eff83411f7a3c1ca6ecec0f6fe926a3e7f
Lorenzo Delgado <ldelgado@zscaler.com>
2022-01-11 17:12:15 +0100

    Opcodes

---
+++ libs/protocol/src/lib/instructions/opcodes.ts
@@ -0,0 +1,77 @@
+// // Drawing instructions
+// export const ARC = 'arc';
+// export const CFILL = 'cfill';
+// export const CLIP = 'clip';
+// export const CLOSE = 'close';
+// export const COPY = 'copy';
+// export const CSTROKE = 'cstroke';
+// export const CURSOR = 'cursor';
+// export const CURVE = 'curve';
+// export const DISPOSE = 'dispose';
+// export const DISTORT = 'distort';
+// export const IDENTITY = 'identity';
+// export const LFILL = 'lfill';
+// export const LINE = 'line';
+// export const LSTROKE = 'lstroke';
+// export const MOVE = 'move';
+// export const POP = 'pop';
+// export const PUSH = 'push';
+// export const RECT = 'rect';
+// export const RESET = 'reset';
+// export const SET = 'set';
+// export const SHADE = 'shade';
+// export const SIZE = 'size';
+// export const START = 'start';
+// export const TRANSFER = 'transfer';
+// export const TRANSFORM = 'transform';
+//
+// // Streaming instructions
+// export const ACK = 'ack';
+// export const ARGV = 'argv';
+// export const AUDIO = 'audio';
+// export const BLOB = 'blob';
+// export const CLIPBOARD = 'clipboard';
+// export const END = 'end';
+// export const FILE = 'file';
+// export const IMG = 'img';
+// export const NEST = 'nest';
+// export const PIPE = 'pipe';
+// export const VIDEO = 'video';
+//
+// // Object instructions
+// export const body = 'body';
+// export const filesystem = 'filesystem';
+// export const get = 'get';
+// export const put = 'put';
+// export const undefine = 'undefine';
+//
+// // Client handshake instructions
+// export const AUDIO = 'audio';
+// export const CONNECT = 'connect';
+// export const IMAGE = 'image';
+// export const SELECT = 'select';
+// export const SIZE = 'size';
+// export const TIMEZONE = 'timezone';
+// export const VIDEO = 'video';
+//
+// // Server handshake instructions
+// export const ARGS = 'args';
+//
+// // Client control instructions
+// export const DISCONNECT = 'disconnect';
+// export const NOP = 'nop';
+// export const SYNC = 'sync';
+//
+// // Server control instructions
+// export const DISCONNECT = 'disconnect';
+// export const ERROR = 'error';
+// export const LOG = 'log';
+// export const MOUSE = 'mouse';
+// export const NOP = 'nop';
+// export const READY = 'ready';
+// export const SYNC = 'sync';
+//
+// // Client events
+// export const KEY = 'key';
+// export const MOUSE = 'mouse';
+// export const SIZE = 'size';
